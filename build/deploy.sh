d258710ad200bd6c0368cc5c1df869c970baf268
pvilaca <pvilaca@192.168.1.69>
2018-06-18 22:25:16 +0100

    [update] travis-ci configurations

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,7 @@
 MEWORKBENCH_VERSION=1.0.3
+
+
+#This variable must be iqual to id in distributionManagement.repository/snapshotRepository on pom.xml
 DEPLOYMENT_REPO_ID="oss.sonatype.org"
 
 
@@ -14,16 +17,21 @@ echo VERSION: $MEWORKBENCH_VERSION
 #echo OSS_SONATYPE_PASS $OSS_SONATYPE_PASS
 
 
+
 H2_HOME=$HOME/.deploym2
+
+echo ${H2_HOME}
 
 M2REPOSITORY=$H2_HOME/repository
 mkdir -p ${M2REPOSITORY}
 cp build/oss.sonatype.settings.xml ${H2_HOME}/settings.xml
 
-sed -i s,LOCAL_M2,$H2_HOME,g ${H2_HOME}/settings.xml
-sed -i s/OSS_SONATYPE_USER/$OSS_SONATYPE_USER/g ${H2_HOME}/settings.xml
-sed -i s/OSS_SONATYPE_PASS/$OSS_SONATYPE_PASS/g ${H2_HOME}/settings.xml
-sed -i s/DEPLOYMENT_REPO_ID/$DEPLOYMENT_REPO_ID/g ${H2_HOME}/settings.xml
+ls ${H2_HOME}
+
+sed -i -e s,LOCAL_M2,$H2_HOME,g ${H2_HOME}/settings.xml
+sed -i -e s/OSS_SONATYPE_USER/$OSS_SONATYPE_USER/g ${H2_HOME}/settings.xml
+sed -i -e s/OSS_SONATYPE_PASS/$OSS_SONATYPE_PASS/g ${H2_HOME}/settings.xml
+sed -i -e s/DEPLOYMENT_REPO_ID/$DEPLOYMENT_REPO_ID/g ${H2_HOME}/settings.xml
 
 #GPG_PRIVATE_KEY=`cat build/gpg_private`
 #echo GPG_PRIVATE_KEY: $GPG_PRIVATE_KEY
